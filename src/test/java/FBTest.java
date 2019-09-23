7077fdf6b25fa1eb57d9664d4bbeddb04783d5bb
Yuliia Horenko <yuliia_horenko@epam.com>
2019-09-23 10:50:24 +0300

    Facebook test  commit

--- src/test/java/FBTest.java
+++ src/test/java/FBTest.java
@@ -7,29 +7,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
 
-public class FBTest{
+public class FBTest {
+
     public static void main(String[] args) {
+        String BaseURL = "https://google.com";
+        String searchWord = "Facebook";
+        String email = "emailforfecebook.moc";
+        String password = "bdgdgdgr";
 
-
-        WebDriver driver;WebDriverManager.chromedriver().setup();
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
 
         driver.manage().window().maximize();
-        driver.get("https://google.com");
-        WebElement search = driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]//input"));
-        search.sendKeys("Facebook");
+        driver.get(BaseURL);
+        WebElement search = driver.findElement(By.name("q"));
+        search.sendKeys(searchWord);
         search.sendKeys(Keys.ENTER);
         WebElement logIn = driver.findElement(By.xpath("//*[@id=\"rso\"]//a"));
         logIn.click();
 
         String URL = driver.getCurrentUrl();
-        Assert.assertEquals(URL, "https://uk-ua.facebook.com/" );
+        Assert.assertEquals(URL, "https://uk-ua.facebook.com/");
 
-        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("emailforfecebook.moc");
-        driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("bdgdgdgr");
+        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys(email);
+        driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys(password);
         driver.findElement(By.xpath("//*[@id=\"loginbutton\"]")).click();
 
         String errorMessage = driver.findElement(By.xpath("//*[@id='error_box']/div[2]")).getText();
         Assert.assertEquals(errorMessage, "Ви робите спроби занадто часто. Будь ласка, спробуйте пізніше.");
+        driver.quit();
     }
 }
